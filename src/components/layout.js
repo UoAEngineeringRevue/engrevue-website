fac0f67eca5f782b2fee5c9c765482f9bb8c6f13
Di Kun Ong <dikuno10two@gmail.com>
2020-02-03 00:04:15 +1300

    Redo layout CSS to incorporate sanitize.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "./sanitize.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
