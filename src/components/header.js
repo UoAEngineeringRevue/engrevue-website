b941a38fc6d53600ecd3197c145bbcfbfe78b127
Di Kun Ong <dikuno10two@gmail.com>
2020-02-04 23:38:51 +1300

    Add progress at nav bar (not responsive)

--- src/components/header.js
+++ src/components/header.js
@@ -5,18 +5,18 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#343a40`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `18px`,
+        display: `flex`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h3 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -24,9 +24,36 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          ENGINEERING REVUE
         </Link>
-      </h1>
+      </h3>
+
+      <nav style={{ margin: `0`, flex: 1, }}>
+        <ul style={{ margin: `0`, display: `flex`, justifyContent: `flex-end` }}>
+          <li style={{ margin: `0 16px` }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              PREVIOUS SHOWS
+            </Link>
+          </li>
+          <li style={{ margin: `0 16px` }}>
+            <Link
+              to="/page-2/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              WATCH NOW
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 )
