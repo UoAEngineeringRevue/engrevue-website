513c42d495bbfa5a5378a3e19a57cc5a73a6b2c1
Alex Ruddell <arud699@aucklanduni.ac.nz>
2020-02-12 23:02:38 +1300

    Previous Revues Page - first commit

---
+++ src/components/prevRevue.js
@@ -0,0 +1,3 @@
+/* This component will be called to load the information from previous revues.
+It will use the (currently named but NEED RENAMING) image.module.css module to format images and data.
+Can keep calling this to call all the info. This .js file will also store all the info of prev revues in it. */
--- src/pages/previous.js
+++ src/pages/previous.js
@@ -3,13 +3,31 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "../components/prevRevue.module.css"
+
+import revue2019 from "../assets/images/previous/2019.jpg"
+import revue2018 from "../assets/images/previous/2018.jpg"
+import revue2017 from "../assets/images/previous/2017.jpg"
+import revue2016 from "../assets/images/previous/2016.jpg"
+import revue2015 from "../assets/images/previous/2015.jpg"
+import revue2014 from "../assets/images/previous/2014.jpg"
+import revue2013 from "../assets/images/previous/2013.jpg"
+import revue2012 from "../assets/images/previous/2012.jpg"
+import revue2011 from "../assets/images/previous/2011.jpg"
+import revue2010 from "../assets/images/previous/2010.jpg"
+import revue2009 from "../assets/images/previous/2009.gif"
+import revue2008 from "../assets/images/previous/2008.gif"
+
 
 const PreviousPage = () => (
   <Layout>
     <SEO title="Previous Revues" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    {/* All my website shiet goes here */}
+    <div>
+      <img src={revue2019} className={styles.format} />
+    </div>
+    
+
   </Layout>
 )
 
