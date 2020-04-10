fdb0d3414fe8eeaaeac0d67860ce5feb419db06d
Alex Ruddell <alex.ruddell@xtra.co.nz>
2020-04-10 13:27:14 +1200

    Failed build resolved

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import PreviousShow from "../components/previousLayout"
 
 import HeroBanner from "../components/index/heroBanner"
 
