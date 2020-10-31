1512cd18379c26556566ba7fbe5c321732c0dd52
Alex Ruddell <alex.ruddell@xtra.co.nz>
2020-10-31 17:20:25 +1300

    Rebuild of sponsorship banner and introduction of 'read more' section. Still to finalise aesthetics and content

--- src/components/sponsor/sponsorBanner.js
+++ src/components/sponsor/sponsorBanner.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./sponsorBanner.module.css"
 import emailIcon from "../../assets/images/icons/email-white-144px.png"
+import downIcon from "../../assets/images/icons/down-button.png"
 
 
 const HeroBanner = () => (
@@ -11,21 +12,26 @@ const HeroBanner = () => (
           Sponsorship
         </h1>
         <p className={styles.subtext}>
-          The generous support we receive from our sponsors enables us to continue pursuing our mission
+          The generous support we receive from our sponsors enables us to continue pursuing our mission;
           to provide a welcoming and inclusive experience that complements the academic components of 
-          Faculty of Engineering studies.
+          studying an Engineering degree at the University of Auckland.
         </p>
         <div className={styles.heroBtnContainer}>
+          <div className={styles.heroBtnVariant}>
+            <a href="#more">
+              <img onClick={window.scrollBy(0,100)} src={downIcon} className={styles.socialImg} alt="See more" /> SEE MORE...
+            </a>
+          </div>
           <div className={styles.heroBtn}>
-            
             <a href="mailto:sponsorship.engrevue@gmail.com">
-              <img src={emailIcon} className={styles.socialImg} alt="Email" /> GET IN TOUCH!
+              <img src={emailIcon} className={styles.socialImg} alt="Email" /> GET IN TOUCH
             </a>
           </div>
         </div>
       </div>
     </div>
   </section>
+  
 )
 
 export default HeroBanner
--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -1,6 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "./sponsor.module.css"
+
+import picture1 from "../assets/images/sponsor/Saturday-23.jpg"
+import picture2 from "../assets/images/sponsor/DSC06330.jpg"
 
 import SponsorBanner from "../components/sponsor/sponsorBanner"
 
@@ -8,7 +12,54 @@ const Sponsor = () => (
   <Layout>
     <SEO title="Sponsor Us" />
       <SponsorBanner />
-    
+      {
+      // =====================================================
+      // TODO(alex-ruddell) Smooth scrolling to "more details"
+      // =====================================================
+      }
+      <section id="more" className={styles.section}>
+        <div class={styles.container}>
+          <div className={styles.rowText}>
+            <div>
+              <h2> OUR LEGACY </h2>
+              <p>
+                For the last 13 years, the Auckland Engineering Revue has continued to grow in presence within the 
+                faculty and has quickly become one of the calendar highlights of the year. The 2020 show was very well
+                received, with a total audience of over 1400 people. Leading into 2021, the Auckland Engineering
+                Revue aims to maintain its strong presence on campus, with a fantastic new cast and a wonderful executive
+                body dedicated to making their best show yet!
+                <br/>
+              </p>
+              <h2> OUR MISSION </h2>
+              <p>
+                Our mission is to bring students together from all year levels and engineering disciplines to form
+                meaningful connections with their peers, provide a rich and rewarding contrast to academic studies,
+                and create a positive, welcoming environment where everyone is given the opportunity to thrive!
+              </p>
+            </div>
+            <div class={styles.imageContainerLeft}>
+              <img src={picture1} className={styles.dispImage} alt="dancing" />
+            </div>
+          </div>
+          
+          <div className={styles.rowText}>
+            <div class={styles.imageContainerRight}>
+              <img src={picture2} className={styles.dispImage} alt="dancing" />
+            </div>            
+            <div>
+              <h2 className={styles.h2alt}> YOUR SUPPORT </h2>
+              <p>
+                Your partnership means everything to us. It represents your support of our mission and helps ensure
+                our show is as accessible and successful as possible!
+                <br/><br/>
+                For further details, <a href="mailto:sponsorship.engrevue@gmail.com">get in touch!</a> We can offer you a 
+                copy of our sponsorship prospectus and a cup of coffee - we'd love to hear from you :)
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      
   </Layout>
 )
 
