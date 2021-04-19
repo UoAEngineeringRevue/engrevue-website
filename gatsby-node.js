13733adc1c8eac63e6d975066de0223f28889dad
Di Kun Ong <dikuno10two@gmail.com>
2021-04-19 15:26:44 +1200

    Add redirects for 2021 sign up forms

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,17 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
-    fromPath: "/interest",
+    fromPath: "/signup",
     toPath:
-      "https://docs.google.com/forms/d/e/1FAIpQLScJ_2Uj8seJS22Q9vov5Qxf6iehP0lBnpEUSOLCKgH29x8D-Q/viewform?usp=sf_link",
+      "https://docs.google.com/forms/d/e/1FAIpQLScWtAsV3LKE63qr-S9_gxV1y-m8IRMJFledCAAPw9zA_rUU5g/viewform",
     isPermanent: true,
     force: true,
-  })
+  }),
+    createRedirect({
+      fromPath: "/crewsignup",
+      toPath:
+        "https://docs.google.com/forms/d/e/1FAIpQLSdKI17wIs1TiwkORyI0w0V-VxLz4RdDDxyCCLn2DI5SXHKRnQ/viewform",
+      isPermanent: true,
+      force: true,
+    })
 }
