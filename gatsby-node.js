836554c5e00d13f9989640cfa9be72194dd047aa
Di Kun Ong <dikuno10two@gmail.com>
2021-07-25 17:10:20 +1200

    Add redirect for iTicket sales

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,9 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
-    fromPath: "/signup",
-    toPath:
-      "https://docs.google.com/forms/d/e/1FAIpQLScWtAsV3LKE63qr-S9_gxV1y-m8IRMJFledCAAPw9zA_rUU5g/viewform",
+    fromPath: "/tickets",
+    toPath: "https://www.iticket.co.nz/events/2021/aug/cs-get-degrease",
     isPermanent: true,
     force: true,
-  }),
-    createRedirect({
-      fromPath: "/crewsignup",
-      toPath:
-        "https://docs.google.com/forms/d/e/1FAIpQLSdKI17wIs1TiwkORyI0w0V-VxLz4RdDDxyCCLn2DI5SXHKRnQ/viewform",
-      isPermanent: true,
-      force: true,
-    })
+  })
 }
