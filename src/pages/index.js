eb4a4b1efe58d52e676fe54f77dc1732d0a30d34
Di Kun Ong <dikun.ong@eroad.com>
2022-07-18 15:00:00 +1200

    Extract Contact Us widgets into component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,69 +6,12 @@ import PromoBanner from "../components/index/promoBanner"
 import * as styles from "./index.module.css"
 
 import AboutUsImage from "../assets/images/index/about-us.jpg"
-import emailIcon from "../assets/images/icons/email-white-144px.png"
-import fbIcon from "../assets/images/icons/facebook-logo-144px.png"
-import instaIcon from "../assets/images/icons/instagram-logo-144px.png"
-import ytIcon from "../assets/images/icons/youtube-logo-144px.png"
-import scIcon from "../assets/images/icons/snapchat-logo-144px.png"
+import ContactUs from "../components/ContactUs"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <PromoBanner />
-    <section className={styles.sectionRegular}>
-      <div className={styles.container} style={{ paddingBottom: "0px" }}>
-        <div className={styles.flavourText}>
-          <h1>
-            7.30pm, August 26th - 28th
-            <br />
-            SkyCity Theatre
-          </h1>
-          <p>
-            Star-crossed lovers Sandy and Danny are devastated as their summer
-            internships come to an end. Torn apart by the distance between their
-            universities, the two engineering students reminisce over those
-            summer nights. But, a surprise reunion brings surprise
-            opportunities, and with it a surprising clash between the lovebirds.
-            Will they be able to overcome their differences and pass the
-            semester? Join Sandy, Danny and the gang at the Engineering Revue to
-            find out!
-          </p>
-        </div>
-
-        <br />
-
-        <p>
-          The annual Auckland Engineering Revue (otherwise known as Auckland’s
-          only show entirely created and performed by Engineering students)
-          presented by MAS is back for its 14th annual performance, and it's
-          sure to be The One That You Want (to watch)!
-          <br />
-          <br />
-          As always, the show ranges from brilliant one-off comedy sketches and
-          fantastic dance numbers to our brilliant jazz band and comedy short
-          films! Whatever you’re looking for, we have it and you’re gonna want
-          to see it!
-          <br />
-          <br />
-          The 2021 Engineering Revue is proudly sponsored by{" "}
-          <a href="https://www.mas.co.nz/" style={{ color: "#b31b20" }}>
-            MAS Insurance
-          </a>{" "}
-          and{" "}
-          <a href="https://nz.marshallday.com/" style={{ color: "#b31b20" }}>
-            Marshall Day Acoustics
-          </a>
-          .
-        </p>
-
-        <div className={styles.flavourBtn}>
-          <a href="https://www.iticket.co.nz/events/2021/aug/cs-get-degrease">
-            CLICK HERE TO BUY TICKETS
-          </a>
-        </div>
-      </div>
-    </section>
     <section className={styles.sectionRegular}>
       <div className={styles.container}>
         <h1>ABOUT US</h1>
@@ -104,75 +47,11 @@ const IndexPage = () => (
             below.
           </p>
 
-          <div className={styles.contactUsSocialContainer}>
-            <a
-              href="mailto:engrevue@gmail.com"
-              className={styles.contactUsSocial}
-            >
-              <img
-                src={emailIcon}
-                className={styles.contactUsSocialIcon}
-                alt="Email"
-              />
-              ENGREVUE@GMAIL.COM
-            </a>
-          </div>
-
-          <div className={styles.contactUsSocialContainer}>
-            <a
-              href="https://www.facebook.com/EngineeringRevue/"
-              className={styles.contactUsSocial}
-            >
-              <img
-                src={fbIcon}
-                className={styles.contactUsSocialIcon}
-                alt="Facebook"
-              />
-              ENGINEERING REVUE
-            </a>
-          </div>
-
-          <div className={styles.contactUsSocialContainer}>
-            <a
-              href="https://www.instagram.com/engrevue"
-              className={styles.contactUsSocial}
-            >
-              <img
-                src={instaIcon}
-                className={styles.contactUsSocialIcon}
-                alt="Instagram"
-              />
-              @ENGREVUE
-            </a>
-          </div>
-
-          <div className={styles.contactUsSocialContainer}>
-            <a
-              href="https://www.youtube.com/c/EngineeringRevue"
-              className={styles.contactUsSocial}
-            >
-              <img
-                src={ytIcon}
-                className={styles.contactUsSocialIcon}
-                alt="YouTube"
-              />
-              ENGINEERING REVUE
-            </a>
-          </div>
-
-          <div className={styles.contactUsSocialContainer}>
-            <a
-              href="https://www.snapchat.com/add/engrevue"
-              className={styles.contactUsSocial}
-            >
-              <img
-                src={scIcon}
-                className={styles.contactUsSocialIcon}
-                alt="Snapchat"
-              />
-              ENGREVUE
-            </a>
-          </div>
+          <ContactUs type="Email" />
+          <ContactUs type="Facebook" />
+          <ContactUs type="Instagram" />
+          <ContactUs type="YouTube" />
+          <ContactUs type="Snapchat" />
         </div>
       </div>
     </section>
