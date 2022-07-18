cac5ff921bbdbef8ceaafb77a4a2a5b3252ec5d3
Di Kun Ong <dikun.ong@eroad.com>
2022-07-19 00:24:54 +1200

    Add proper 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,24 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import background from "../assets/images/404-bg.jpg"
+
 const NotFoundPage = () => (
-  <Layout>
+  <Layout bgImage={background} bgTint>
     <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section
+      style={{
+        color: "white",
+        display: "grid",
+        flex: `1`,
+        placeContent: "start center",
+        marginTop: "120px",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontSize: `48px` }}>NOT FOUND</h1>
+      <p>Like the clothing in Boys' Dance, this page doesn&#39;t exist.</p>
+    </section>
   </Layout>
 )
 
