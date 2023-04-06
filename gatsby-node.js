89a39602a47c91bcc70cdfb8fac515d01b4b5a49
perhapsity <87929939+perhapsity@users.noreply.github.com>
2023-04-06 17:20:26 +1200

    Update gatsby-node.js
    OOPS wrong link

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
     fromPath: "/auditions",
-    toPath: "https://forms.gle/a9WfjFZwWiixHoNm9",
+    toPath: "https://forms.gle/Gdzm41nodG3wvaYY9",
     isPermanent: true,
     redirectInBrowser: true,
     force: true,
