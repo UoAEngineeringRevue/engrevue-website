1868f13d840c8d534056d7df0b474293812a01ae
perhapsity <87929939+perhapsity@users.noreply.github.com>
2023-04-06 17:21:30 +1200

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,15 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
-    fromPath: "/auditions",
+    fromPath: "/cast",
     toPath: "https://forms.gle/Gdzm41nodG3wvaYY9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
+  createRedirect({
+    fromPath: "/crew",
+    toPath: "https://forms.gle/a9WfjFZwWiixHoNm9",
     isPermanent: true,
     redirectInBrowser: true,
     force: true,
