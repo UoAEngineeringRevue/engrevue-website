09bb67ee1c5265a97da704d2cf3c3bb4b68d18d2
perhapsity <87929939+perhapsity@users.noreply.github.com>
2023-04-06 17:17:01 +1200

    Update gatsby-node.js
    removed old links

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,6 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
   createRedirect({
-    fromPath: "/distrussed",
-    toPath: "https://www.youtube.com/watch?v=u7XGILPI7Lg",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/dancewithus",
-    toPath: "https://www.youtube.com/watch?v=f4-92UYWsVs",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/interview",
-    toPath: "https://www.youtube.com/watch?v=GmS0LObyRD0",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/tickets",
-    toPath: "https://www.iticket.co.nz/events/2022/aug/charlie-internship-lottery",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
     fromPath: "/auditions",
     toPath: "https://forms.gle/a9WfjFZwWiixHoNm9",
     isPermanent: true,
