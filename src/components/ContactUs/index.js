bc4ca291453c3564b233324f2142f13890d7b97b
perhapsity <87929939+perhapsity@users.noreply.github.com>
2023-04-06 19:00:15 +1200

    Update index.js

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -6,7 +6,7 @@ import EmailIcon from "../../assets/images/icons/email-white-144px.png"
 import FacebookIcon from "../../assets/images/icons/facebook-logo-144px.png"
 import InstagramIcon from "../../assets/images/icons/instagram-logo-144px.png"
 import YouTubeIcon from "../../assets/images/icons/youtube-logo-144px.png"
-import SnapchatIcon from "../../assets/images/icons/snapchat-logo-144px.png"
+import SnapchatIcon from "../../assets/images/icons/snapchatwhite-logo-144px.png"
 import TikTokIcon from "../../assets/images/icons/tiktok-logo-144px.png"
 
 const ContactUs = ({ type }) => {
