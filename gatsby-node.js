870be30d2f157d476c3b077c013f00b1011e6f2d
perhapsity <87929939+perhapsity@users.noreply.github.com>
2023-10-23 23:04:39 +1300

    Update gatsby-node.js
    removed ticket links

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,18 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
-  createRedirect({
-    fromPath: "/tickets",
-    toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/ticket",
-    toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/lecture",
-    toPath: "https://docs.google.com/presentation/d/19jLpCe-OLfRCgKeDhqFU-aM3zPPn0iqNl7lpb_8b-Z4/edit?usp=sharing",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
+  // createRedirect({
+  //   fromPath: "/tickets",
+  //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
+  //   isPermanent: true,
+  //   redirectInBrowser: true,
+  //   force: true,
+  // })
+  // createRedirect({
+  //   fromPath: "/ticket",
+  //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
+  //   isPermanent: true,
+  //   redirectInBrowser: true,
+  //   force: true,
+  // })
+  // ^^ update these links once you've set up the tickets!
 }
