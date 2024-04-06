3eed47606803a02b97c2d8b1ec1356ffbf8b3517
EngineeringRevue <166195612+EngineeringRevue@users.noreply.github.com>
2024-04-06 17:01:55 +1300

    Update gatsby-node.js
    2024 sign ups

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,19 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
+  createRedirect({
+    fromPath: "/cast",
+    toPath: "https://forms.gle/Ypb9K6717Nk3GVXw9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
+  createRedirect({
+    fromPath: "/crew",
+    toPath: "https://forms.gle/HUXL5Xg3xZCihPJi9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
   // createRedirect({
   //   fromPath: "/tickets",
   //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
