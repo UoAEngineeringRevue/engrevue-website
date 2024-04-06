6dc94547c5f24f7e808f12b9e9ed39d01198bbaa
EngineeringRevue <166195612+EngineeringRevue@users.noreply.github.com>
2024-04-06 17:02:47 +1300

    Update gatsby-node.js
    2024 signups

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,5 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
-  createRedirect({
-    fromPath: "/cast",
-    toPath: "https://forms.gle/Ypb9K6717Nk3GVXw9",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/crew",
-    toPath: "https://forms.gle/HUXL5Xg3xZCihPJi9",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
   // createRedirect({
   //   fromPath: "/tickets",
   //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
@@ -30,3 +16,17 @@ exports.createPages = ({ actions }) => {
   // })
   // ^^ update these links once you've set up the tickets!
 }
+  createRedirect({
+    fromPath: "/cast",
+    toPath: "https://forms.gle/Ypb9K6717Nk3GVXw9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
+  createRedirect({
+    fromPath: "/crew",
+    toPath: "https://forms.gle/HUXL5Xg3xZCihPJi9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
