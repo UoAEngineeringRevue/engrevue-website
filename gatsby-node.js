691e91fe6467ad9f862a589239fee6ca10ddbf58
EngineeringRevue <166195612+EngineeringRevue@users.noreply.github.com>
2025-04-27 23:00:11 +1200

    Update gatsby-node.js for auditions
    Added audition links for 2025!

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,40 @@
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
-    createRedirect({
-    fromPath: "/tickets",
-    toPath: "http://www.iticket.co.nz/events/2024/aug/phineas-and-ferb",
-    isPermanent: true,
-    redirectInBrowser: true,
-    force: true,
-  })
+  //   createRedirect({
+  //   fromPath: "/tickets",
+  //   toPath: "http://www.iticket.co.nz/events/2024/aug/phineas-and-ferb",
+  //   isPermanent: true,
+  //   redirectInBrowser: true,
+  //   force: true,
+  // })
       createRedirect({
     fromPath: "/lecture",
-    toPath: "https://www.canva.com/design/DAGM4R5kKPQ/e-Py2AdNzHGmpZ6H_E6bfg/edit?utm_content=DAGM4R5kKPQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    toPath: "https://www.canva.com/design/DAGl0TyH9Ms/RZWwIG8ThBjX25nK-PohcA/view?utm_content=DAGl0TyH9Ms&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6afd9725e6,
     isPermanent: true,
     redirectInBrowser: true,
     force: true,
   })
- // createRedirect({
- //   fromPath: "/crew",
- //   toPath: "https://forms.gle/HUXL5Xg3xZCihPJi9",
- //   isPermanent: true,
- //   redirectInBrowser: true,
- //   force: true,
- // })
-  // createRedirect({
-  //   fromPath: "/tickets",
-  //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
-  //   isPermanent: true,
-  //   redirectInBrowser: true,
-  //   force: true,
-  // })
-  // createRedirect({
-  //   fromPath: "/ticket",
-  //   toPath: "http://www.iticket.co.nz/events/2023/aug/engineering-revue",
-  //   isPermanent: true,
-  //   redirectInBrowser: true,
-  //   force: true,
-  // })
+ createRedirect({
+   fromPath: "/crew",
+   toPath: "https://forms.gle/bHDsf4osiec4uNZ98",
+   isPermanent: true,
+   redirectInBrowser: true,
+   force: true,
+ })
+  createRedirect({
+    fromPath: "/cast",
+    toPath: "https://forms.gle/vgZ2csiwE7USrhsC9",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
+  createRedirect({
+    fromPath: "/links",
+    toPath: "https://linktr.ee/engrevue",
+    isPermanent: true,
+    redirectInBrowser: true,
+    force: true,
+  })
   // ^^ update these links once you've set up the tickets!
 }
 
